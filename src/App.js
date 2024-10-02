a734ad223e779e1afd4bb65fae6ebcf738e72ba9
prajyot <narwareprajyot25@gmail.com>
2024-10-02 22:58:57 +0530

    14

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
@@ -6,22 +7,36 @@ import { useState } from "react";
 
 function App() {
   const [mode, setMode] = useState("light"); //Whether darkMode is enabledor not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert=(message,type) => {
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
   const toggleMode = () => {
     if (mode === "light") {
       setMode ('dark');
-      document.body.style.backgroundColor='grey'
+      document.body.style.backgroundColor='#01185d'
+      showAlert("dark mode has been enabled","success")
     }
     else{
       setMode('light'); 
       document.body.style.backgroundColor='white'
+      showAlert("light mode has been enabled","success")
 
     }
   };
   return (
     <>
       <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
       <div className="container">
-        <TextForm heading="Enter the text to analyze below" mode={mode} />
+        <TextForm heading="Enter the text to analyze below" Alert={showAlert} mode={mode} />
         {/* <About /> Fix: Use correct JSX syntax */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,13 +10,14 @@ export default function TextForm(props) {
     // setText("You have clicked on handleUpClick");
     let newtext=text.toUpperCase();
     setText(newtext);
-    
+    props.Alert("Converted to UpperCase","success")
   }
   const handleLoClick =() => {
     // console.log('upper case was clicked');
     // setText("You have clicked on handleUpClick");
     let newtext=text.toLowerCase();
     setText(newtext);
+    props.Alert("Converted to LowerCase","success")
     
   }
 
@@ -24,6 +25,8 @@ export default function TextForm(props) {
     let msg = new SpeechSynthesisUtterance();
     msg.text = text;
     window.speechSynthesis.speak(msg);
+    props.Alert("your message is being spoken","success")
+
   }
 //funtion to handle changes made by user in the textarea
   const handleOnChange =(event) => {
@@ -36,7 +39,7 @@ export default function TextForm(props) {
         <div>
       <div className='container' style={{color:props.mode==='dark' ? 'white':'black'}}>
         <h1>{props.heading}</h1>
-        <textarea className="form-control my-3" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='light' ? 'white':'grey' ,color:props.mode==='dark' ? 'white':'black'}}  id="myBox" rows="8"></textarea>
+        <textarea className="form-control my-3" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='light' ? 'white':'#01185d' ,color:props.mode==='dark' ? 'white':'black'}}  id="myBox" rows="8"></textarea>
       </div>
         </div>
       <button className='btn btn-primary mx-2' onClick={handleUpClick} >Convert to UpperCase</button>
