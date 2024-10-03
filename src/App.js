97be93a0d19e61f01e36688b9c1908f6f95290d4
prajyot <narwareprajyot25@gmail.com>
2024-10-03 18:08:20 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import './App.css';
-import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About'
+import "./App.css";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { useState } from "react";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); // Whether darkMode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#01185d";
+      showAlert("dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode has been enabled", "success");
+    }
+  };
+
   return (
     <>
-      <Navbar title="Textutils" />
-      <div className="container">
-        {/* <TextForm heading="Enter the text to analyze below" /> */}
-        <About /> {/* Fix: Use correct JSX syntax */}
-      </div>
+      <Router>
+        <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm heading="Enter the text to analyze below" Alert={showAlert} mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
